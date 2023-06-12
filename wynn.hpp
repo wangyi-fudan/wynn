8f2e241d48e3742d510d5104b8fbc569c38aea7c
Yi Wang <godspeed_china@yeah.net>
2023-06-12 14:56:11 +0800

    update

--- wynn.hpp
+++ wynn.hpp
@@ -14,10 +14,10 @@ using	namespace	std;
 using	namespace	Eigen;
 namespace	wynn{
 struct	global{
-	unsigned	threads;	//	number of threads
-	uint64_t	prng;	//	prng seed
-	float	eta;	//	learning rate
-	global(){	set_threads(omp_get_num_procs()>1?omp_get_num_procs()-1:1);	prng=time(NULL);	eta=1.0f/(1ull<<16);	}
+	unsigned	threads;
+	uint64_t	prng;
+	float	learning_rate;
+	global(){	set_threads(omp_get_num_procs()>1?omp_get_num_procs()-1:1);	prng=time(NULL);	learning_rate=1.0f/(1ull<<16);	}
 	void	set_threads(unsigned	n){	threads=n;	omp_set_num_threads(threads);	setNbThreads(threads);	}
 }global;
 static	inline	uint64_t	wyrand(uint64_t	*seed){	*seed+=0xa0761d6478bd642full;	uint64_t	see1=*seed^0xe7037ed1a0b428dbull;	see1*=(see1>>32)|(see1<<32);	return	(*seed*((*seed>>32)|(*seed<<32)))^((see1>>32)|(see1<<32));	}
@@ -26,10 +26,10 @@ static	inline	float	wy2gau(uint64_t	r){	const	float	_wynorm=1.0/(1ull<<20);	retu
 static	inline	void	wymum(uint64_t	*A,	uint64_t	*B){	uint64_t	hh=(*A>>32)*(*B>>32),	hl=(*A>>32)*(uint32_t)*B,	lh=(uint32_t)*A*(*B>>32),	ll=(uint64_t)(uint32_t)*A*(uint32_t)*B;	*A=((hl>>32)|(hl<<32))^hh;	*B=((lh>>32)|(lh<<32))^ll;}
 static	inline	uint64_t	wyhash64(uint64_t	A,	uint64_t	B){	A^=0xa0761d6478bd642full;	B^=0xa0761d6478bd642full;	wymum(&A,&B);	A^=0xa0761d6478bd642full;	B^=0xa0761d6478bd642full;	wymum(&A,&B);	return	A^B;}
 template<unsigned	n>
-static	inline	void	tiger(float	*w,	float	*g,	float	*m,	float	r){
-	float	r2=r*2;
-	#pragma	omp	parallel	for
-	for(unsigned	i=0;	i<n;	i++){	m[i]+=0.03125f*(g[i]-m[i]);	w[i]-=r2*((m[i]>0)-0.5f);	}
+static	inline	void	tiger(float	*w,	float	*g,	float	*m,	float	lr){
+	float	r=lr*2;
+	#pragma omp parallel for
+	for(unsigned	i=0;	i<n;	i++){	m[i]+=0.03125f*(g[i]-m[i]);	w[i]-=r*((m[i]>0)-0.5f);	}
 }
 }
 #endif
